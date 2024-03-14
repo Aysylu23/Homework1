72e507fd72b063f5b8b7b7c3429707d280ba198b
Aysylu <aysylu_bad@mail.ru>
2024-03-14 16:02:46 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿/* Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a * b, 
+ введенные из командной строки, и выводящую результат выполнения на экран.*/
+
+
+namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                int sum = 0;
+                int num1 = Convert.ToInt32(args[0]);
+                string symbol = args[1];
+                int num2 = Convert.ToInt32(args[2]);
+
+                if (symbol == "+") 
+                {
+                    sum = num1 + num2;  
+                }
+                if (symbol == "-")
+                {
+                    sum = num1 - num2;
+                }
+                if (symbol == "*")
+                {
+                    sum = num1 * num2;
+                }
+                if (symbol == "/")
+                {
+                    sum = num1 / num2;
+                }
+                Console.WriteLine(sum);
+            }
+                
+        }
+    }
+}
